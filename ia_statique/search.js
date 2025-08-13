3da7da4d4d060048e43e03ce841aa66d277d0bcc
Entreprise SHOPNET <Entrepriseshopia@gmail.com>
2025-08-13 01:42:06 +0100

    Ajout dossier ia_statique à la racine

--- ia_statique/search.js
+++ ia_statique/search.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const pool = require('../Backende/db');
+const pool = require('../db');
 
 const categoryMap = {
   'bébé': ['baby', 'enfant', 'kid', 'clothing', 'poussette'],
